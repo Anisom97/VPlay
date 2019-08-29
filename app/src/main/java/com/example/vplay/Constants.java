a1612de401189dbd6a602df418fd87d3f660d06a
corphish <d97.avinaba@gmail.com>
2019-08-29 22:30:31 +0530

    SignIn: Use new network API
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/src/main/java/com/example/vplay/Constants.java
+++ app/src/main/java/com/example/vplay/Constants.java
@@ -1,6 +1,6 @@
 package com.example.vplay;
 
 public class Constants {
-    public static final String TEST_URL = "http://192.168.0.105";
+    public static final String TEST_URL = "http://192.168.0.106";
     public static final String SERVER_QUERY_ERROR = "__ERROR";
 }
--- app/src/main/java/com/example/vplay/SignInPage.java
+++ app/src/main/java/com/example/vplay/SignInPage.java
@@ -1,5 +1,6 @@
 package com.example.vplay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,7 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.example.vplay.network.NetworkQueryTask;
+import com.example.vplay.network.interfaces.OnNetworkQueryCompletionListener;
+import com.example.vplay.network.models.NetworkResponse;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,15 +26,17 @@ public class SignInPage extends AppCompatActivity {
     Button loginButton;
     RadioGroup rg;
 
+    final String[] tableNames = {"admintest", "teamtest", "playertest"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-        emailid=(EditText)findViewById(R.id.EnterIdSignIn);
-        passwordL= (EditText)findViewById(R.id.PasswordSignIn);
+        emailid = findViewById(R.id.EnterIdSignIn);
+        passwordL = findViewById(R.id.PasswordSignIn);
 
-        loginButton=(Button)findViewById(R.id.loginButton);
+        loginButton= findViewById(R.id.loginButton);
 
         final List<Integer> radioButtonList = Arrays.asList(
                 R.id.admin,
@@ -37,16 +49,25 @@ public class SignInPage extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String username = emailid.getText().toString();
+                String password = passwordL.getText().toString();
 
-                String username= emailid.getText().toString();
-                String password= passwordL.getText().toString();
+                List<NameValuePair> entity = new ArrayList<>();
 
-                String type = "login";
-                BackgroundWorker backgroundWorker = new BackgroundWorker(SignInPage.this);
-                backgroundWorker.execute(type, username, password, "" + radioButtonList.indexOf(rg.getCheckedRadioButtonId()));
+                entity.add(new BasicNameValuePair("user_name", username));
+                entity.add(new BasicNameValuePair("password", password));
+                entity.add(new BasicNameValuePair("table", tableNames[radioButtonList.indexOf(rg.getCheckedRadioButtonId())]));
 
+                NetworkQueryTask networkQueryTask = new NetworkQueryTask(Constants.TEST_URL + "/" + "signin.php", "POST", new OnNetworkQueryCompletionListener() {
+                    @Override
+                    public void onNetworkQueryCompleted(@NonNull NetworkResponse networkResponse) {
+                        if (networkResponse.isError())
+                            Toast.makeText(SignInPage.this, networkResponse.getErrorMessage(), Toast.LENGTH_LONG).show();
+                        else Toast.makeText(SignInPage.this, "Success", Toast.LENGTH_LONG).show();
+                    }
+                }, entity);
+                networkQueryTask.execute();
             }
         });
     }
-
 }
