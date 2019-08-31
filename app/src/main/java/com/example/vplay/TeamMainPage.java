ee2ef4e32a4618060ba1afc747214a90a4f4fcde
corphish <d97.avinaba@gmail.com>
2019-08-31 19:46:41 +0530

    TeamMainPage: Initial data fetch
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/src/main/java/com/example/vplay/TeamMainPage.java
+++ app/src/main/java/com/example/vplay/TeamMainPage.java
@@ -1,8 +1,20 @@
 package com.example.vplay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.vplay.network.NetworkQueryTask;
+import com.example.vplay.network.interfaces.OnNetworkQueryCompletionListener;
+import com.example.vplay.network.models.NetworkResponse;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TeamMainPage extends AppCompatActivity {
 
@@ -10,5 +22,17 @@ public class TeamMainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teammainpage);
+
+        List<NameValuePair> l = new ArrayList<>();
+        l.add(new BasicNameValuePair("query", "select * from teamtest;"));
+
+        NetworkQueryTask networkQueryTask = new NetworkQueryTask(Constants.TEST_URL + "/" + "dbQuery.php", "POST", new OnNetworkQueryCompletionListener() {
+            @Override
+            public void onNetworkQueryCompleted(@NonNull NetworkResponse networkResponse) {
+                Log.d("VPlay", networkResponse.getResponseMessage().toString());
+            }
+        }, l);
+
+        networkQueryTask.execute();
     }
 }
