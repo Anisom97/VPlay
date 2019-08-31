dd1235220259770dac9e1f9142258893b1d5b41d
corphish <d97.avinaba@gmail.com>
2019-08-31 19:44:56 +0530

    SignInPage: Add sign in validation
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/src/main/java/com/example/vplay/SignInPage.java
+++ app/src/main/java/com/example/vplay/SignInPage.java
@@ -3,6 +3,7 @@ package com.example.vplay;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +28,7 @@ public class SignInPage extends AppCompatActivity {
     RadioGroup rg;
 
     final String[] tableNames = {"admintest", "teamtest", "playertest"};
+    final Class[] activities = {PlayerMainPage.class, TeamMainPage.class, AdminMainPage.class};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,18 +54,26 @@ public class SignInPage extends AppCompatActivity {
                 String username = emailid.getText().toString();
                 String password = passwordL.getText().toString();
 
+                final int selection = radioButtonList.indexOf(rg.getCheckedRadioButtonId());
+
                 List<NameValuePair> entity = new ArrayList<>();
 
                 entity.add(new BasicNameValuePair("user_name", username));
                 entity.add(new BasicNameValuePair("password", password));
-                entity.add(new BasicNameValuePair("table", tableNames[radioButtonList.indexOf(rg.getCheckedRadioButtonId())]));
+                entity.add(new BasicNameValuePair("table", tableNames[selection]));
 
                 NetworkQueryTask networkQueryTask = new NetworkQueryTask(Constants.TEST_URL + "/" + "signin.php", "POST", new OnNetworkQueryCompletionListener() {
                     @Override
                     public void onNetworkQueryCompleted(@NonNull NetworkResponse networkResponse) {
                         if (networkResponse.isError())
                             Toast.makeText(SignInPage.this, networkResponse.getErrorMessage(), Toast.LENGTH_LONG).show();
-                        else Toast.makeText(SignInPage.this, "Success", Toast.LENGTH_LONG).show();
+                        else {
+                            if ("1".equals(networkResponse.getResponseMessage().get(0))) {
+                                startActivity(new Intent(SignInPage.this, activities[selection]));
+                            } else {
+                                Toast.makeText(SignInPage.this, "Invalid username/password", Toast.LENGTH_LONG).show();
+                            }
+                        }
                     }
                 }, entity);
                 networkQueryTask.execute();
